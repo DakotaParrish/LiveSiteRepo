06bfa0b47ea28f6499a606d5e1bf5c82ddcd126e
Dakota Parrish <needforspeeddj@gmail.com>
2022-03-11 19:05:29 -0500

    fixed login bug

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -148,7 +148,7 @@
         console.log("Home Page");
         $("#AboutUsButton").on("click", () => 
         {
-            location.href = "/about";
+           LoadLink("about");
         });
 
         $("main").append(`<p id="MainParagraph" class="mt-3">This is the Main Paragraph</p>`);
@@ -411,6 +411,8 @@
                 (
                     `<a class="nav-link" data="login"><i class="fas fa-sign-in-alt"></i> Login</a>`
                 );
+
+                AddNavigationEvents();
                 
                 // redirect back to login page
                 LoadLink("login");
